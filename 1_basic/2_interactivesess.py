3865aa34edf438e24be674848aae0910514624ce
wangxiaoxuegit <51707555+wangxiaoxuegit@users.noreply.github.com>
2019-12-23 16:25:32 +0800

    Create 2_interactivesess.py

---
+++ 1_basic/2_interactivesess.py
@@ -0,0 +1,12 @@
+
+import tensorflow as tf
+sess = tf.InteractiveSession()
+
+x = tf.Variable([1., 2.])
+a = tf.constant([3., 3.])
+
+# initial variable
+x.initializer.run()
+
+sub = tf.subtract(x, a)
+print(sub.eval())
